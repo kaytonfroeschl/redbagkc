11066119f742cbd761698253f63f93ee2edeca56
Bill Froeschl <bfroeschl@gmail.com>
2023-05-23 09:21:45 -0500

    add

--- src/ui-components/ChildList.js
+++ src/ui-components/ChildList.js
@@ -1,9 +1,26 @@
+import { Flex, Table, TableCell, TableRow } from '@aws-amplify/ui-react';
+
 function ChildList() {
    
     return (
-        <table>            
-            Child List Goes Here
-        </table>
+        <Flex direction="row">            
+            <Table size="small">
+            <TableRow className='Header' fontStyle={'italic'} fontSize={'small'}>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Age</TableCell>
+                <TableCell>Red Bag Lady</TableCell>
+                <TableCell>Sponsor</TableCell>
+            </TableRow >
+            <TableRow className='Data' >
+            <   TableCell>A123</TableCell>
+                <TableCell>Jamal</TableCell>
+                <TableCell>8</TableCell>
+                <TableCell>Kayton Froeschl</TableCell>
+                <TableCell>Bill Froeschl</TableCell>
+            </TableRow>
+            </Table>
+        </Flex>
     )
 };
 
