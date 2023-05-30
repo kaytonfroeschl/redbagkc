c411ab7153dc54b44b859915b0909781a49a353b
Kayton Froeschl <kaytonfroeschl@Kaytons-MacBook-Pro-2.local>
2023-05-30 04:32:23 -0400

    add

--- src/ui-components/ChildList.js
+++ src/ui-components/ChildList.js
@@ -1,27 +1,19 @@
 import { Flex, Table, TableCell, TableRow } from '@aws-amplify/ui-react';
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { ListChildrenQuery } from '../graphql/queries/getAllChildren';
+
+
+async function fetchChildren() {
+    const apiData = await API.graphql({ query: ListChildrenQuery });
+    console.log(apiData);
+}
 
 function ChildList() {
-   
+    fetchChildren();
     return (
-        <Flex direction="row">            
-            <Table size="small">
-            <TableRow className='Header' fontStyle={'italic'} fontSize={'small'}>
-                <TableCell>ID</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Age</TableCell>
-                <TableCell>Red Bag Lady</TableCell>
-                <TableCell>Sponsor</TableCell>
-            </TableRow >
-            <TableRow className='Data' >
-            <   TableCell>A123</TableCell>
-                <TableCell>Jamal</TableCell>
-                <TableCell>8</TableCell>
-                <TableCell>Kayton Froeschl</TableCell>
-                <TableCell>Bill Froeschl</TableCell>
-            </TableRow>
-            </Table>
-        </Flex>
-    )
+        <div>Child List goes here</div>
+    );
+    
 };
 
 export default ChildList;
